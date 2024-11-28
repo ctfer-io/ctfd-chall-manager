578c568338430e16b53194088575bb5579783b24
Nicolas FAUGEROUX <nicolas.faugeroux@protonmail.com>
2024-11-28 23:17:00 +0100

    feat: permit destroy instance on flag configuration

--- assets/view.js
+++ assets/view.js
@@ -320,27 +320,3 @@ CTFd._internal.challenge.restart = function() {
     });
     
 }
-CTFd._internal.challenge.submit = function (preview) {
-    var challenge_id = CTFd._internal.challenge.data.id;
-    var submission = $('#challenge-input').val()
-
-    var body = {
-        'challenge_id': challenge_id,
-        'submission': submission,
-    }
-    var params = {}
-    if (preview)
-        params['preview'] = true
-
-    return CTFd.api.post_challenge_attempt(params, body).then(function (response) {
-        if (response.status === 429) {
-            // User was ratelimited but process response
-            return response
-        }
-        if (response.status === 403) {
-            // User is not logged in or CTF is paused.
-            return response
-        }
-        return response
-    })
-};
