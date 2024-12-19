b9085bcefb0e63c68bbfff0d894de092477608b5
Nicolas FAUGEROUX <nicolas.faugeroux@protonmail.com>
2024-12-19 23:50:41 +0100

    impr(lock): fix RedisRWLock and add ThreadingRWLock

--- utils/locker.py
+++ utils/locker.py
@@ -1,25 +1,92 @@
-import redis
+import threading
 import os
-from redis.exceptions import LockError
-import redis.lock
-
 from .logger import configure_logger
+import redis
+from redis.lock import Lock
+from redis.exceptions import LockError
 
 logger = configure_logger(__name__)
 
-# Get the Redis connection URL from the environment variable
 REDIS_URL = os.getenv('REDIS_URL')
-if REDIS_URL == None:
-    logger.error(f"REDIS_URL not found, quitting")
-    raise Exception("REDIS_URL not found, quitting")
+redis_client = None
+if REDIS_URL:
+    redis_client = redis.Redis.from_url(REDIS_URL)
+    logger.info("Redis lock configured successfully")
+else:
+    logger.info("Local lock configured successfully")
 
-logger.debug(f"REDIS_URL is {REDIS_URL}")
+class ThreadingRWLock:
+    def __init__(self, name):
+        self.name = name
 
-# Connect to the Redis server using the URL
-redis_client = redis.Redis.from_url(REDIS_URL)
-logger.debug(f"redis client configured succesfully")
+        self.m1 = threading.Lock()
+        self.m2 = threading.Lock()
+        self.m3 = threading.Lock()
 
-class RWLock():
+        self.r = threading.Lock()
+        self.w = threading.Lock()
+
+        self.rcounter = int(0)
+        self.wcounter = int(0)
+
+    def r_lock(self):
+        try:
+            self.m3.acquire()
+            self.r.acquire()
+            self.m1.acquire()
+
+            self.rcounter = self.rcounter + 1
+
+            if self.rcounter == 1:
+                self.w.acquire()
+
+        finally:
+            self.m1.release()
+            self.r.release()
+            self.m3.release()
+
+    def r_unlock(self):
+        try:
+            self.m1.acquire()
+
+            self.rcounter = self.rcounter - 1
+
+            if self.rcounter == 0:
+                self.w.release()
+
+        finally:
+            self.m1.release()
+ 
+    def rw_lock(self):
+        try:
+            self.m2.acquire()
+
+            self.wcounter = self.wcounter + 1 
+
+            if self.wcounter == 1:
+                self.r.acquire()            
+
+        finally:
+            self.m2.release()
+            self.w.acquire()
+            
+
+    def rw_unlock(self):
+        try:
+            self.w.release()
+            self.m2.acquire()
+
+            self.wcounter = self.wcounter - 1
+
+            if self.wcounter == 0:
+                self.r.release()          
+
+        finally:
+            self.m2.release()
+
+
+
+class RedisRWLock():
     def __init__(self, name):
         self.name = name
 
@@ -27,67 +94,66 @@ class RWLock():
 
         self.m1 = redis_client.lock(name=f"{name}_m1")
         self.m2 = redis_client.lock(name=f"{name}_m2")
+        self.m3 = redis_client.lock(name=f"{name}_m3")
 
-        self.r = redis_client.lock(name=f"{name}_r")
-        self.w = redis_client.lock(name=f"{name}_w")
-
-        self.g = redis_client.lock(name=f"{name}_g") # no concurrency reader
-
+        self.r = redis_client.lock(name=f"{name}_r", thread_local=False)
+        self.w = redis_client.lock(name=f"{name}_w", thread_local=False)
 
     def r_lock(self):
-        logger.debug(f"{self.name}_r locked")
-        logger.debug(f"{redis_client.get(f'{self.name}_readcount')}")
         try:
-
+            self.m3.acquire()
             self.r.acquire()
             self.m1.acquire()
 
+            if redis_client.get(f"{self.name}_readcount") == None:
+                redis_client.set(f"{self.name}_readcount", 0)
+
             redis_client.incr(f"{self.name}_readcount")
 
-            if redis_client.get(f"{self.name}_readcount") == 1:
+            if int(redis_client.get(f"{self.name}_readcount")) == 1:
                 self.w.acquire()
-
-            self.m1.release()
-            self.r.release()
-
-            self.g.acquire()
 
         except LockError as e:
             logger.warning(f"Failed to acquire lock due to error: {str(e)}")
 
+        finally:
+            self.m1.release()
+            self.r.release()
+            self.m3.release()
 
     def r_unlock(self):
-        logger.debug(f"{self.name}_r unlocked")
         try:
-            self.g.release()
-
             self.m1.acquire()
 
             redis_client.decr(f"{self.name}_readcount")
 
-            if redis_client.get(f"{self.name}_readcount") == 0:
+            if int(redis_client.get(f"{self.name}_readcount")) == 0:
                 self.w.release()
 
+        except LockError as e:
+            logger.warning(f"Failed to release lock due to error: {str(e)}")
+
+        finally:
             self.m1.release()
 
-        except LockError as e:
-            logger.warning(f"Failed to acquire lock due to error: {str(e)}")
-
     def rw_lock(self):
-        logger.debug(f"{self.name}_rw locked")
         try:
             self.m2.acquire()
 
+            if redis_client.get(f"{self.name}_writecount") == None:
+                redis_client.set(f"{self.name}_writecount", 0)
+
             redis_client.incr(f"{self.name}_writecount")
 
-            if redis_client.get(f"{self.name}_writecount") == 1:
-                self.r.acquire()
-
-            self.m2.release()
-            self.w.acquire()
+            if int(redis_client.get(f"{self.name}_writecount")) == 1:
+                self.r.acquire()            
 
         except LockError as e:
             logger.warning(f"Failed to acquire lock due to error: {str(e)}")
+
+        finally:
+            self.m2.release()
+            self.w.acquire()    
 
     def rw_unlock(self):
         logger.debug(f"{self.name}_rw unlocked")
@@ -96,10 +162,20 @@ class RWLock():
             self.m2.acquire()
 
             redis_client.decr(f"{self.name}_writecount")
-            if redis_client.get(f"{self.name}_writecount") == 0:
+            if int(redis_client.get(f"{self.name}_writecount")) == 0:
                 self.r.release()
 
+        except LockError as e:
+            logger.warning(f"Failed to release lock due to error: {str(e)}")
+
+        finally:
             self.m2.release()
 
-        except LockError as e:
-            logger.warning(f"Failed to acquire lock due to error: {str(e)}")
+class RWLock():
+    def __new__(cls, name):
+        if REDIS_URL:
+            logger.debug(f"initiate RedisRWLock for {name}")
+            return RedisRWLock(name)
+        else:
+            logger.debug(f"initiate ThreadingRWLock for {name}")
+            return ThreadingRWLock(name)
