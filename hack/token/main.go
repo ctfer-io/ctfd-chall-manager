38460f238c53d2949f0d30403f0f64a52b332b03
nicolas <nicolas.faugeroux@protonmail.com>
2025-07-14 17:13:13 +0200

    chore: remove comments

--- hack/token/main.go
+++ hack/token/main.go
@@ -20,14 +20,12 @@ func main() {
 	name := os.Getenv("CTFD_NAME")
 	password := os.Getenv("CTFD_PASSWORD")
 
-	//fmt.Println("[+] Getting initial nonce and session values")
 	nonce, session, err := api.GetNonceAndSession(url)
 	if err != nil {
 		log.Fatalf("Getting nonce and session: %s", err)
 	}
 	client := api.NewClient(url, nonce, session, "")
 
-	//fmt.Println("[+] Logging in")
 	if err := client.Login(&api.LoginParams{
 		Name:     name,
 		Password: password,
@@ -36,7 +34,6 @@ func main() {
 	}
 
 	// Create API Key
-	//fmt.Println("[+] Creating API Token")
 	token, err := client.PostTokens(&api.PostTokensParams{
 		Expiration:  "2222-01-01",
 		Description: "Github Workflow CI API token.",
