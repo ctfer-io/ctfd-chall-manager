aaf27f862ef5f11f06beb9275bd9c6fbefccc122
nicolas <nicolas.faugeroux@protonmail.com>
2026-03-17 00:01:14 +0100

    fix: until and timeout update

--- utils/chall_manager_error.py
+++ utils/chall_manager_error.py
@@ -47,3 +47,11 @@ def chall_manager_exception_builder(resp: requests.Response) -> ChallManagerExce
     )
 
     return cm_exception
+
+
+class ChallManagerPluginException(Exception):
+    """
+    Internal plugin exception
+    """
+
+    pass
