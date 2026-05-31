bb407a5e8eabedf2befa17872f76c4fa02e20aea
nicolas <nicolas.faugeroux@protonmail.com>
2026-05-31 22:44:56 +0200

    chore: lint

--- test/test_api_admin_import.py
+++ test/test_api_admin_import.py
@@ -11,40 +11,38 @@ from .utils import config, create_challenge
 
 
 # pylint: disable=invalid-name,missing-timeout,duplicate-code
-class Test_F_AdminInstance(unittest.TestCase):
+class Test_F_AdminImport(unittest.TestCase):
     """
-    Test_F_AdminInstance defines all tests cases for the /admin/import endpoint.
+    Test_F_AdminImport defines all tests cases for the /admin/import endpoint.
     """
 
     def test_user_connection_is_denied(self):
         """
         Performs calls on admin endpoint with user account.
-        Must be denied on all.
+        Must be denied.
         """
         r = requests.post(
             f"{config.plugin_url}/admin/import", headers=config.headers_user
         )
         self.assertEqual(r.status_code, 403)
 
-
     def test_import_missing_challenge(self):
         """
-        Performs test to create an instance for a challenge that exists
-        and for a source that exists.
+        Check that I can import a missing challenge.
         """
 
         challengeId = create_challenge()
 
         # delete it in chall-manager
-        r = requests.delete(f"http://localhost:8080/api/v1/challenge/{challengeId}") # chall-manager 
+        r = requests.delete(
+            f"http://localhost:8080/api/v1/challenge/{challengeId}"
+        )  # chall-manager
         self.assertEqual(r.status_code, 200)
 
-        payload = {
-            "challengeId": challengeId
-        }
+        payload = {"challengeId": challengeId}
 
         r = requests.post(
-            f"{config.plugin_url}/admin/import", 
+            f"{config.plugin_url}/admin/import",
             headers=config.headers_admin,
             data=json.dumps(payload),
         )
@@ -52,27 +50,26 @@ class Test_F_AdminInstance(unittest.TestCase):
 
     def test_import_existing_challenge(self):
         """
-        Performs test to create an instance for a challenge that exists
-        and for a source that exists.
+        Check that I cannot import an existing challenge.
         """
 
         challengeId = create_challenge()
 
-        payload = {
-            "challengeId": challengeId
-        }
+        payload = {"challengeId": challengeId}
 
         r = requests.post(
-            f"{config.plugin_url}/admin/import", 
+            f"{config.plugin_url}/admin/import",
             headers=config.headers_admin,
             data=json.dumps(payload),
         )
-        self.assertEqual(r.status_code, 409) # already exists
-
+        self.assertEqual(r.status_code, 409)  # already exists
 
     def test_import_wihtout_challenge_id(self):
+        """
+        Check that I can't import a missing challengeId
+        """
         r = requests.post(
-            f"{config.plugin_url}/admin/import", 
+            f"{config.plugin_url}/admin/import",
             headers=config.headers_admin,
         )
         self.assertEqual(r.status_code, 400)
